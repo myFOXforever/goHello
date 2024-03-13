e14413afcaa00fdbf177d24e20f8594d4143b2d0
fox <zengchunlin01@gmail.com>
2024-03-13 20:25:03 +0800

    change package

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
-package sayhello
+package goHello
 
 import "fmt"
 
+// SayHello Just say hello
 func SayHello(name string) {
 	fmt.Println("Hello", name)
 }
+
 func SayBye(name string) {
 	fmt.Println("bye", name)
 }
